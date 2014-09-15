a02efc009192bf5ca5e2c87627875ced2dc423ed
rgraham15 <rosemary__kelly@hotmail.com>
2014-09-15 16:55:51 -0600

    Created component library Code
    Created entity SpecialEvent - created context class, and created
    component library interface. (DAL, Entities, and BLL folders)

---
+++ eRestaurantSystem/BLL/eRestaurantController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#region
+using eRestaurantSystem.Entities;
+using eRestaurantSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    public class eRestaurantController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<SpecialEvent> SpecialEvent_List()
+        {
+            //interfacing with our context class.DbContext, ties into entity framework. 
+            using (eRestaurantContext context = new eRestaurantContext()) 
+            {
+                return context.SpecialEvents.ToList();
+            }
+        }
+    }
+}
---
+++ eRestaurantSystem/Entities/SpecialEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace eRestaurantSystem.Entities
+{
+    public class SpecialEvent
+    {
+        [Key]
+        public char EventCode { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
